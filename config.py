1bdb120f96409d14d3d36695a065f3f88e14cee5
DLEGENDOP <140735285+DLEGENDOP@users.noreply.github.com>
2023-07-28 07:51:50 +0530

    Update config.py
    Signed-off-by: DLEGENDOP <140735285+DLEGENDOP@users.noreply.github.com>

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "2059751904").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/DLEGENDOP/AnonXMusic/tree/master")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/DLEGENDOP/AnonXMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
